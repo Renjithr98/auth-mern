a8a886a23a9bf53f39f825abb4b2daf24ee52aff
Renjthr98 <renjithe6883@gamil.com>
2023-11-24 10:36:01 +0530

    complete sign up page ui

--- api/index.js
+++ api/index.js
@@ -3,39 +3,27 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-import cookieParser from 'cookie-parser';
-import path from 'path';
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-const __dirname = path.resolve();
+mongoose.connect(process.env.MONGO)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.log(err);
+});
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
-
 app.use(express.json());
 
-app.use(cookieParser());
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(3000,() =>{
+  console.log('server listening on port 3000');
 });
 
-app.use('/api/user', userRoutes);
+app.use("/api/user", userRoutes);
 app.use('/api/auth', authRoutes);
+
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
